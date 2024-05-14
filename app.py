b28ff337146a670c833e4e2550509e828f91717d
Ildi Szabo <szabo.ildikomse@gmail.com>
2024-05-14 11:48:53 +0200

    added random speech functionality

--- app.py
+++ app.py
@@ -4,18 +4,11 @@ import requests
 # Polclassifier frontend
 '''
 
-predict_url = "https://polclassifier-g4mow3fhiq-ew.a.run.app/predict"
-random_speech_url = "https://polclassifier-g4mow3fhiq-ew.a.run.app/predict_speech"
-
-##### Guessing the party #####
-api_form = st.form(key='api_form')
-speech = api_form.text_input(label="Political speech (400-600 words):", value="")
-submitted = api_form.form_submit_button(label='Guess the party!')
+predict_url = "https://svm-pvutvs4yla-ew.a.run.app/predict"
+random_speech_url = "https://svm-pvutvs4yla-ew.a.run.app/speech"
 
 
-# Make the API request when the button is pushed
-def make_request(url, params):
-    party_key = {
+party_code_to_name = {
         "Con": "Conservative Party",
         "Lab": "Labour Party",
         "LibDem": "Liberal Democrats",
@@ -24,19 +17,51 @@ def make_request(url, params):
         "UUP": "Ulster Unionist Party",
         "PlaidCymru": "Plaid Cymru"
     }
-
-    response = requests.get(url=url, params=params)
-    result = response.json()
-    return f"""{party_key[result['party']]}"""
-
-if submitted:
-    params = {
-        "speech": speech,
-    }
-
-    response = make_request(predict_url, params)
-    st.markdown(f'### {response}!')
-    st.balloons()
+party_name_to_code = {v: k for k,v in party_code_to_name.items()}
 
 
 ##### Retrieving a random speech to test the app on #####
+st.markdown(f"### If you don\'t have a speech ready, we can get a random one for you!")
+
+api_form_random = st.form(key='api_form_random')
+party = api_form_random.selectbox(label="Party:", options=party_name_to_code.keys())
+submitted_random = api_form_random.form_submit_button(label='Give me a random speech from this party!')
+
+
+def make_request_random(url, params):
+    response = requests.get(url=url, params=params)
+    result = response.json()
+    return f"""{result["speech"]}"""
+
+if submitted_random:
+    params_random = {
+        "party": party_name_to_code[party],
+    }
+
+    response = make_request_random(random_speech_url, params_random)
+    st.markdown(f"#### Here's your speech:")
+    st.markdown(f'{response}')
+    st.balloons()
+
+
+##### Guessing the party #####
+st.markdown(f'\n\n\n ### Let us guess what party gave your speech!')
+api_form_predict = st.form(key='api_form_predict')
+speech = api_form_predict.text_input(label="Your political speech (400-600 words):", value="")
+submitted_predict = api_form_predict.form_submit_button(label='Guess the party!')
+
+
+# Make the API request when the button is pushed
+def make_request_predict(url, params, parties):
+    response = requests.get(url=url, params=params)
+    result = response.json()
+    return f"""{parties[result['party']]}"""
+
+if submitted_predict:
+    params_predict = {
+        "speech": speech,
+    }
+
+    response = make_request_predict(predict_url, params_predict, party_code_to_name)
+    st.markdown(f'### {response}!')
+    st.balloons()
